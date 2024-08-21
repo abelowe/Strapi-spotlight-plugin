99f1ee682cbb5de669f18ad4d2dfb0e4f5a047f7
unknown <40187949@roctilburg.nl>
2024-08-21 04:02:28 +0200

    gedupliceerde knop gefixed

--- my-new-project/src/plugins/spotlight/admin/src/pages/HomePage/index.js
+++ my-new-project/src/plugins/spotlight/admin/src/pages/HomePage/index.js
@@ -7,11 +7,9 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import pluginId from '../../pluginId';
-import SpotlightSearchBar from '../../components/SpotlightSearchBar';
 const HomePage = () => {
   return (
     <div>
-      <SpotlightSearchBar /> 
     </div>
   );
 };
